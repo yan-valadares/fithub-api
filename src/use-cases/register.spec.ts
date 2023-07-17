50244a4f3a45c48839a9f47cce72bd969c86afd2
Yan Valadares <yancardoso@outlook.com.br>
2023-07-17 16:06:22 -0300

    Criação de testes unitários

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,59 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    // In memory test database Pattern
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password on registry', async () => {
+    // In memory test database Pattern
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    const isPasswordCorretlyHashed = await compare('123456', user.password_hash)
+
+    expect(isPasswordCorretlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    // In memory test database Pattern
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@email.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    await expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
